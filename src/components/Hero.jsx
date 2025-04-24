a6eba310d67391b1402889a9c31d3d75630ccf54
Preet Chaudhary <preetchaudhary224@gmail.com>
2025-04-25 01:44:16 +0530

    fixed animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const  Hero = () => {
     <div className='flex flex-wrap'>
       <div className='w-full lg:w-1/2'>
         <div className="flex flex-col p-9 items-center lg:items-start">
-            <motion.h1 animate={{x:100}}  className='pb-16 text-6xl text-cyan-100 font-thin tracking-tight lg: mt-16 lg:text-8xl'>
+            <motion.h1 animate={{x:8}}  className='pb-16 text-6xl text-cyan-100 font-thin tracking-tight lg: mt-16 lg:text-8xl'>
                 Preet Chaudhary
             </motion.h1>
                 <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'> Full Stack Developer</span>
